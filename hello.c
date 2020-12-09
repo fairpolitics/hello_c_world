9aea38e69ceaa28110dd5383e5c4174f9fc0fadf
fairpolitics <fairpolitics@me.com>
2020-12-09 15:39:04 +0900

    time

--- hello.c
+++ hello.c
@@ -5,14 +5,14 @@ void main()
 {
   printf("Hello World\n");
   time_t t;
-  struct tm* localtime(const time_t *timer);
+  struct tm* jstTm;
   time(&t);
   jstTm = localtime(&t);
   printf("%04d/%02d/%02d %02d:%02d:%02d \n",
     jstTm->tm_year + 1900,
     jstTm->tm_mon + 1,
     jstTm->tm_mday,
-    jstTm->tm_hour,
+    jstTm->tm_hour + 9,
     jstTm->tm_min,
     jstTm->tm_sec);
 }
